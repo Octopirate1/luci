71eaf1ce067a0a74323ef27b184e45185014f345
Octopirate1 <octopirateyt@gmail.com>
2020-07-24 17:40:36 -0700

    preframeupdate & postframeupdate structs added

--- luci.h
+++ luci.h
@@ -27,6 +27,7 @@ void dump_mem(void *memp, size_t offset, int len);
 	//
 
 
+typedef float float_t;
 typedef bool bool_t;
 typedef enum ELEMENT_TYPE { ET_New = 0, ET_Char, ET_String, ET_Int, ET_Float, ET_Array, ET_Object } element_type_t;
 
@@ -66,14 +67,20 @@ void element_list_recurse(element_t *listp, int indent);
 void free_elements(element_t *listp);
 element_t *find_element_by_name(element_t *listp, char *namep);
 
+
+
+#define VERSION_LENGTH	4
+#define PORT_COUNT			4
+#define NAMETAG_LENGTH	8
+#define CHAR_COUNT			2
+
+
+
 typedef struct GAME_START game_start_t;
 typedef struct GAME_START_PORT game_start_port_t;
 typedef struct GAME_INFO_BLOCK game_info_block_t; // technically a uint8[312], but struct makes it far more clear what everything is, + can be passed into python as an object
 typedef struct GAME_INFO_BLOCK_PORT game_info_block_port_t; // technically a uint8[312], but struct makes it far more clear what everything is, + can be passed into python as an object
 
-#define VERSION_LENGTH	4
-#define PORT_COUNT		3	//technically 4 but arrays are 0indexed
-#define NAMETAG_LENGTH	8
 struct GAME_START {
 	uint8_t version[VERSION_LENGTH];
 	game_info_block_t *game_info_block;
@@ -103,7 +110,7 @@ struct GAME_INFO_BLOCK {
 	uint8_t item_spawn_bitfield_3;
 	uint8_t item_spawn_bitfield_4;
 	uint8_t item_spawn_bitfield_5;
-	float damage_ratio;
+	float_t damage_ratio;
 	game_info_block_port_t *ports[PORT_COUNT];
 };
 
@@ -117,7 +124,195 @@ struct GAME_INFO_BLOCK_PORT {
 	uint8_t team_id;
 	uint8_t player_bitfield;
 	uint8_t cpu_level;
-	float offense_ratio;
-	float defense_ratio;
-	float model_scale;
+	float_t offense_ratio;
+	float_t defense_ratio;
+	float_t model_scale;
+};
+
+typedef struct __attribute__((__packed__)) GSFULLBLOCK  {
+	uint8_t event_type;	// offset 0x0
+	uint8_t version[VERSION_LENGTH]; // offset 0x1
+	uint8_t	game_bitfield_1;	// offset 0x5
+	uint8_t	game_bitfield_2;	// 0x6
+	uint8_t game_bitfield_3;	// 0x7
+	uint8_t _pad0[5]; // 5 byte skip
+	uint8_t is_teams;	// 0xD
+	uint8_t _pad1[2]; // 2 byte skip
+	int8_t item_freq; // 0x10
+	int8_t sd_value; // 0x11
+	uint8_t _pad; // 1 byte skip
+	uint16_t stage; // 0x13
+	uint32_t timer_value; // 0x15
+	uint8_t _pad2[15]; // 15 byte skip
+	uint8_t item_spawn_bitfield_1; // 0x29
+	uint8_t item_spawn_bitfield_2;
+	uint8_t item_spawn_bitfield_3;
+	uint8_t item_spawn_bitfield_4;
+	uint8_t item_spawn_bitfield_5; // 0x2C
+	uint8_t _pad3[8]; // 8 byte skip
+	uint32_t damage_ratio; // 0x35
+	uint8_t _pad4[0x2C]; // 0x2C byte skip
+	struct __attribute__((__packed__)) {
+		uint8_t extern_char_id; // 0x65 + 0x24i
+		uint8_t player_type; // 0x66 + 0x24i
+		uint8_t stock_start_count; // 0x67 + 0x24i
+		uint8_t costume_index; // 0x68 + 0x24i
+		uint8_t _pad5[3]; // 3 byte skip
+		uint8_t team_shade; // 0x6C + 0x24i
+		uint8_t handicap; // 0x6D + 0x24i
+		uint8_t team_id; // 0x6E + 0x24i
+		uint8_t _pad6[2]; // 2 byte skip
+		uint8_t player_bitfield; // 0x71 + 0x24i
+		uint8_t _pad7[2]; // 2 byte skip
+		uint8_t cpu_level; // 0x74 + 0x24i
+		uint8_t _pad8[4]; // 4 byte skip
+		uint32_t offense_ratio; // 0x79 + 0x24i
+		uint32_t defense_ratio; // 0x7D + 0x24i
+		uint32_t model_scale; // 0x81 + 0x24i
+		uint8_t _pad9[4]; // need to get to 24 bytes within port struct, 4 byte skip
+	} gib_port[PORT_COUNT]; // ends at 0xF5
+	uint8_t _pad10[2*0x24]; // infospace for 6 but slippi supports 4 because whatever so byte skip
+	uint32_t random_seed;	// offset 0x13D
+	struct __attribute__((__packed__)) {
+		uint32_t dashback_fix;	// offset 0x141 + 0x8i
+		uint32_t shield_drop_fix;	// offset 0x145 + 0x8i
+	} gsb_port[PORT_COUNT]; // ends at 0x161
+	uint16_t nametag[PORT_COUNT][NAMETAG_LENGTH];	// offset 0x161, 0x10*PORT_COUNT length
+	uint8_t pal; // 0x1A1
+	uint8_t frozen_ps; // 0x1A2
+} gsfullblock_t;
+
+
+
+typedef struct PRE_FRAME_UPDATE pre_frame_update_t;
+
+struct PRE_FRAME_UPDATE {
+	int32_t frame_number;
+	uint8_t player_index;
+	bool_t is_follower;
+	uint32_t random_seed;
+	uint16_t action_state_id;
+	float_t x_position;
+	float_t y_position;
+	float_t facing_direction;
+	float_t ctrlstick_x;
+	float_t ctrlstick_y;
+	float_t cstick_x;
+	float_t cstick_y;
+	float_t trigger;
+	uint32_t processed_buttons;
+	uint16_t physical_buttons;
+	float_t physical_l_trigger;
+	float_t physical_r_trigger;
+	uint8_t ucf_x_analog;
+	float_t damage_percent;
+};
+
+typedef struct __attribute__((__packed__)) PREFIFULLBLOCK  {
+	uint8_t event_type;	// offset 0x0
+	int32_t frame_number; // 0x1
+	uint8_t player_index; // 0x5
+	uint8_t is_follower; // 0x6
+	uint32_t random_seed; // 0x7
+	uint16_t action_state_id; // 0xB
+	uint32_t x_position; // 0xD
+	uint32_t y_position; // 0x11
+	uint32_t facing_direction; // 0x15
+	uint32_t ctrlstick_x; // 0x19
+	uint32_t ctrlstick_y; // 0x1D
+	uint32_t cstick_x; // 0x21
+	uint32_t cstick_y; // 0x25
+	uint32_t trigger; // 0x29
+	uint32_t processed_buttons; // 0x2D
+	uint16_t physical_buttons; // 0x31
+	uint32_t physical_l_trigger; // 0x33
+	uint32_t physical_r_trigger; // 0x37
+	uint8_t ucf_x_analog; // 0x3B
+	uint32_t damage_percent; // 0x3C
+} prefifullblock_t;
+
+
+
+typedef struct POST_FRAME_UPDATE post_frame_update_t;
+
+struct POST_FRAME_UPDATE {
+	int32_t frame_number;
+	uint8_t player_index;
+	bool_t is_follower;
+	uint8_t intern_char_id;
+	uint16_t action_state_id;
+	float_t x_position;
+	float_t y_position;
+	float_t facing_direction;
+	float_t damage_percent;
+	float_t shield_size;
+	uint8_t last_hit_id;
+	uint8_t current_combo_count;
+	uint8_t last_hit_by;
+	uint8_t stocks_remaining;
+	float_t action_state_frames;
+	uint8_t state_bitflags_1;
+	uint8_t state_bitflags_2;
+	uint8_t state_bitflags_3;
+	uint8_t state_bitflags_4;
+	uint8_t state_bitflags_5;
+	float_t misc_as;
+	bool_t ground_state;
+	uint16_t last_ground_id;
+	uint8_t jumps_remaining;
+	uint8_t lcancel_status;
+	uint8_t hurtbox_collision_state;
+	float_t si_air_x_speed;
+	float_t si_air_y_speed;
+	float_t attack_air_x_speed;
+	float_t attack_air_y_speed;
+	float_t si_ground_x_speed;
+
+};
+
+typedef struct __attribute__((__packed__)) POSTFIFULLBLOCK  {
+	uint8_t event_type;	// offset 0x0
+	int32_t frame_number; // 0x1
+	uint8_t player_index; // 0x5
+	uint8_t is_follower; // 0x6
+	uint8_t intern_char_id; // 0x7
+	uint16_t action_state_id; // 0x8
+	uint32_t x_position; // 0xA
+	uint32_t y_position; // 0xE
+	uint32_t facing_direction; // 0x12
+	uint32_t damage_percent; // 0x16
+	uint32_t shield_size; // 0x1A
+	uint8_t last_hit_id; // 0x1E
+	uint8_t current_combo_count; // 0x1F
+	uint8_t last_hit_by; // 0x20
+	uint8_t stocks_remaining; // 0x21
+	uint32_t action_state_frames; // 0x22
+	uint8_t state_bitflags_1; // 0x26
+	uint8_t state_bitflags_2;
+	uint8_t state_bitflags_3;
+	uint8_t state_bitflags_4;
+	uint8_t state_bitflags_5; // 0x2A
+	uint32_t misc_as; // 0x2B
+	uint8_t ground_state; // 0x2F
+	uint16_t last_ground_id; // 0x30
+	uint8_t jumps_remaining; // 0x32
+	uint8_t lcancel_status; // 0x33
+	uint8_t hurtbox_collision_state; // 0x34
+	uint32_t si_air_x_speed; // 0x35
+	uint32_t si_air_y_speed; // 0x39
+	uint32_t attack_air_x_speed; // 0x3D
+	uint32_t attack_air_y_speed; // 0x41
+	uint32_t si_ground_x_speed; // 0x45
+} postfifullblock_t;
+
+typedef struct FRAME_OBJ frame_obj_t;
+
+struct FRAME_OBJ {
+	struct {
+		struct {
+			pre_frame_update_t *preframep;
+			post_frame_update_t *postframep;
+		} char_frames[CHAR_COUNT]; // 0 is leader, 1 is follower or NULL pointers if no follower
+	} ports[PORT_COUNT]; // NULL value if no player on port
+	frame_obj_t *nextp;
 };
--- luci_process_raw.c
+++ luci_process_raw.c
@@ -5,8 +5,10 @@
 #include <arpa/inet.h>
 
 static size_t process_game_start(uint8_t *p, game_start_t *gamestartp);
+static size_t process_pre_frame_update(uint8_t *p, pre_frame_update_t *preframep);
+static size_t process_post_frame_update(uint8_t *p, post_frame_update_t *postframep);
 static size_t process_event(uint8_t *p);
-typedef enum { EVENT_PAYLOADS = 0x35, EVENT_GAME_START=0x36 } event_t;
+typedef enum { EVENT_PAYLOADS = 0x35, EVENT_GAME_START = 0x36, EVENT_PRE_FRAME_UPDATE = 0x37, EVENT_POST_FRAME_UPDATE = 0x38, EVENT_GAME_END = 0x39 } event_t;
 
 void process_raw_data(void *ptr, size_t len)
 {
@@ -14,6 +16,8 @@ void process_raw_data(void *ptr, size_t len)
 	uint8_t *currentp = p;
 	size_t offset = 0;
 	event_t type;
+
+	// pre_frame_update_t *prevpreframep = NULL;
 
 	// DBG(printf("Object offset=0x%lx : %c\n", offset, type););
 
@@ -37,8 +41,16 @@ void process_raw_data(void *ptr, size_t len)
 				event_size = process_event(currentp); // should be first object, then never encountered again
 				break;
 			case EVENT_GAME_START:;
-				game_start_t *gamestartp = malloc(sizeof(game_start_t));
+				game_start_t *gamestartp = (game_start_t *)malloc(sizeof(game_start_t));
 				event_size = process_game_start(currentp, gamestartp);
+				break;
+			case EVENT_PRE_FRAME_UPDATE:;
+				pre_frame_update_t *preframep = (pre_frame_update_t *)malloc(sizeof(pre_frame_update_t));
+				event_size = process_pre_frame_update(currentp, preframep);
+				break;
+			case EVENT_POST_FRAME_UPDATE:;
+				post_frame_update_t *postframep = (post_frame_update_t *)malloc(sizeof(post_frame_update_t));
+				event_size = process_post_frame_update(currentp, postframep);
 				break;
 			default:;
 				event_size = 1;
@@ -64,60 +76,17 @@ static size_t process_event(uint8_t *p)
 	return (p[1]+1);
 }
 
+static float ntohf(uint32_t net32)
+{
+    union {
+        float f;
+        uint32_t u;
+    } value;
 
+    value.u = ntohl(net32);
 
-typedef struct __attribute__((__packed__)) GSFULLBLOCK  {
-	uint8_t event_type;	// offset 0x0
-	uint8_t version[VERSION_LENGTH]; // offset 0x1
-	uint8_t	game_bitfield_1;	// offset 0x5
-	uint8_t	game_bitfield_2;	// 0x6
-	uint8_t game_bitfield_3;	// 0x7
-	uint8_t _pad1[4]; // 5 byte skip
-	uint8_t isteams;	// 0xD
-	uint8_t item_freq; // 0x10
-	uint8_t sd_value; // 0x11
-	uint8_t _pad2; // 1 byte skip
-	uint16_t stage; // 0x13
-	uint32_t timer_value; // 0x15
-	uint8_t item_spawn_bitfield_1; // 0x29
-	uint8_t item_spawn_bitfield_2;
-	uint8_t item_spawn_bitfield_3;
-	uint8_t item_spawn_bitfield_4;
-	uint8_t item_spawn_bitfield_5; // 0x2C
-	uint8_t _pad3[7]; // 8 byte skip
-	float damage_ratio; // 0x35
-	uint8_t _pad4[0x2C]; // 0x2C byte skip
-	struct __attribute__((__packed__)) gib_port {
-		uint8_t extern_char_id; // 0x65 + 0x24i
-		uint8_t player_type; // 0x66 + 0x24i
-		uint8_t stock_start_count; // 0x67 + 0x24i
-		uint8_t costume_index; // 0x68 + 0x24i
-		uint8_t _pad5[2]; // 3 byte skip
-		uint8_t team_shade; // 0x6C + 0x24i
-		uint8_t handicap; // 0x6D + 0x24i
-		uint8_t team_id; // 0x6E + 0x24i
-		uint8_t _pad6[1]; // 2 byte skip
-		uint8_t player_bitfield; // 0x71 + 0x24i
-		uint8_t _pad7[1]; // 2 byte skip
-		uint8_t cpu_level; // 0x74 + 0x24i
-		uint8_t _pad8[3]; // 4 byte skip
-		float offense_ratio; // 0x79 + 0x24i
-		float defense_ratio; // 0x7D + 0x24i
-		float model_scale; // 0x81 + 0x24i
-		uint8_t _pad9[3]; // need to get to 24 bytes within port struct
-	}[PORT_COUNT]; // ends at 0x119
-	uint8_t _pad9[0x24]; // uhhhhhh byte skip but maybe unneccesary?
-	uint32_t random_seed;	// offset 0x13D
-	struct __attribute__((__packed__)) gsb_port {
-		uint32_t dashback_fix;	// offset 0x141 + 0x8i
-		uint32_t shield_drop_fix;	// offset 0x145 + 0x8i
-		uint32_t nametag[NAMETAG_LENGTH];	// offset 0x161 + 0x10i
-	}[PORT_COUNT];
-	uint8_t pal;
-	uint8_t frozen_ps;
-} gsfullblock_t;
-
-
+    return value.f;
+}
 
 static size_t process_game_start(uint8_t *p, game_start_t *gamestartp)
 {
@@ -126,24 +95,48 @@ static size_t process_game_start(uint8_t *p, game_start_t *gamestartp)
 	if (ibp->event_type != EVENT_GAME_START) return (0);
 
 	game_info_block_t *gameinfoblockp = (game_info_block_t*)malloc(sizeof(game_info_block_t));
-	game_info_block_port_t *gameibportp = (game_info_block_port_t*)malloc(sizeof(game_info_block_port_t));
 
 	game_start_port_t *gamestartportp[PORT_COUNT];
 	game_info_block_port_t *gameibportp[PORT_COUNT];
 
-	if (gameinfoblockp == NULL || gamestartportp == NULL || gameibportp == NULL) goto malloc_fail;
+	if (gamestartp == NULL || gameinfoblockp == NULL || gamestartportp == NULL || gameibportp == NULL) goto malloc_fail;
 
+	for(int i = 0;i<PORT_COUNT;i++){
+		gamestartportp[i] = (game_start_port_t*)malloc(sizeof(game_start_port_t));
+
+		gamestartportp[i]->dashback_fix = (uint32_t)ntohl(ibp->gsb_port[i].dashback_fix);
+		gamestartportp[i]->shield_drop_fix = (uint32_t)ntohl(ibp->gsb_port[i].shield_drop_fix);
+		for (int j=0; j<NAMETAG_LENGTH; j++) gamestartportp[i]->nametag[j] = (uint16_t)ntohs(ibp->nametag[i][j]);
+
+		gameibportp[i] = (game_info_block_port_t*)malloc(sizeof(game_info_block_port_t));
+
+		gameibportp[i]->extern_char_id = ibp->gib_port[i].extern_char_id;
+		gameibportp[i]->player_type = ibp->gib_port[i].player_type;
+		gameibportp[i]->stock_start_count = ibp->gib_port[i].stock_start_count;
+		gameibportp[i]->costume_index = ibp->gib_port[i].costume_index;
+		gameibportp[i]->team_shade = ibp->gib_port[i].team_shade;
+		gameibportp[i]->handicap = ibp->gib_port[i].handicap;
+		gameibportp[i]->team_id = ibp->gib_port[i].team_id;
+		gameibportp[i]->player_bitfield = ibp->gib_port[i].player_bitfield;
+		gameibportp[i]->cpu_level = ibp->gib_port[i].cpu_level;
+		gameibportp[i]->offense_ratio = ntohf(ibp->gib_port[i].offense_ratio); // FLOATS!! AAAAARGHHH have to do bs ntohf or smth
+		gameibportp[i]->defense_ratio = ntohf(ibp->gib_port[i].defense_ratio);
+		gameibportp[i]->model_scale = ntohf(ibp->gib_port[i].model_scale);
+	};
+
+	memcpy(gamestartp->version, ibp->version, VERSION_LENGTH); // don't need to byte flip bc uint8 so no for loop lulz
 	gamestartp->game_info_block = gameinfoblockp;
-
-
-	gamestartp->version = ibp->version;
+	gamestartp->random_seed = (uint32_t)ntohl(ibp->random_seed);
+	memcpy(gamestartp->ports, gamestartportp, PORT_COUNT);
+	gamestartp->pal = (bool_t)ibp->pal;
+	gamestartp->frozen_ps = (bool_t)ibp->frozen_ps;
 
 	gameinfoblockp->game_bitfield_1 = ibp->game_bitfield_1;
 	gameinfoblockp->game_bitfield_2 = ibp->game_bitfield_2;
 	gameinfoblockp->game_bitfield_3 = ibp->game_bitfield_3;
 	gameinfoblockp->is_teams = (bool_t)ibp->is_teams;
-	gameinfoblockp->item_freq = ibp->item_freq;
-	gameinfoblockp->sd_value = ibp->sd_value;
+	gameinfoblockp->item_freq = (int8_t)ibp->item_freq;
+	gameinfoblockp->sd_value = (int8_t)ibp->sd_value;
 	gameinfoblockp->stage = (uint16_t)ntohs(ibp->stage);
 	gameinfoblockp->timer_value = (uint32_t)ntohl(ibp->timer_value);
 	gameinfoblockp->item_spawn_bitfield_1 = ibp->item_spawn_bitfield_1;
@@ -151,25 +144,94 @@ static size_t process_game_start(uint8_t *p, game_start_t *gamestartp)
 	gameinfoblockp->item_spawn_bitfield_3 = ibp->item_spawn_bitfield_3;
 	gameinfoblockp->item_spawn_bitfield_4 = ibp->item_spawn_bitfield_4;
 	gameinfoblockp->item_spawn_bitfield_5 = ibp->item_spawn_bitfield_5;
+	gameinfoblockp->damage_ratio = ntohf(ibp->damage_ratio); //ntohf
+	memcpy(gameinfoblockp->ports, gameibportp, PORT_COUNT);
 
-	for(int i = 0;i<PORT_COUNT;i++){
-		gamestartportp[i] = (game_start_port_t*)malloc(sizeof(game_start_port_t));
-
-		gamestartportp[i]->dashback_fix = (uint32_t)ntohl(ibp->gsb_port[i].dashback_fix);
-		gamestartportp[i]->shield_drop_fix = (uint16_t)ntohs(ibp->gsb_port[i].shield_drop_fix);
-		gamestartportp[i]->nametag = (uint32_t)ntohl(ibp->gsb_port[i].dashback_fix); //fix bc array
-
-		gameibportp[i] = (game_info_block_port_t*)malloc(sizeof(game_info_block_port_t));
-	};
-
-
-
-	gamestartp->random_seed = (uint32_t)ntohl(ibp->random_seed);
-
-
-	printf("Offset for random_seed = 0x%p\n", &(((infoblock_t*)0)->random_seed));
+	// printf("Offset for pal = %p\n", &(((gsfullblock_t*)0)->pal)); // oll korrect
 
 	return 0x1a2;
+
+	malloc_fail:; //we have to allocate quite a bit of heap so this goto is useful here for oneline if statements
+		DBG(printf("malloc failed\n"););
+		// free_elements(elemlistp);
+		return (0);
+}
+
+static size_t process_pre_frame_update(uint8_t *p, pre_frame_update_t *preframep)
+{
+	prefifullblock_t *ibp = (prefifullblock_t *)p;
+	if (ibp->event_type != EVENT_PRE_FRAME_UPDATE) return (0);
+
+	if (preframep == NULL) goto malloc_fail;
+
+	preframep->frame_number = (int32_t)ntohl(ibp->frame_number);
+	preframep->player_index = ibp->player_index;
+	preframep->is_follower = (bool_t)ibp->is_follower;
+	preframep->random_seed = (uint32_t)ntohl(ibp->random_seed);
+	preframep->action_state_id = (uint16_t)ntohs(ibp->action_state_id);
+	preframep->x_position = ntohf(ibp->x_position);
+	preframep->y_position = ntohf(ibp->y_position);
+	preframep->facing_direction = ntohf(ibp->facing_direction);
+	preframep->ctrlstick_x = ntohf(ibp->ctrlstick_x);
+	preframep->ctrlstick_y = ntohf(ibp->ctrlstick_y);
+	preframep->cstick_x = ntohf(ibp->cstick_x);
+	preframep->cstick_y = ntohf(ibp->cstick_y);
+	preframep->trigger = ntohf(ibp->trigger);
+	preframep->processed_buttons = (uint32_t)ntohl(ibp->processed_buttons);
+	preframep->physical_buttons = (uint16_t)ntohs(ibp->physical_buttons);
+	preframep->physical_l_trigger = ntohf(ibp->physical_l_trigger);
+	preframep->physical_r_trigger = ntohf(ibp->physical_r_trigger);
+	preframep->ucf_x_analog = ibp->ucf_x_analog;
+	preframep->damage_percent = ntohf(ibp->damage_percent);
+
+	return 0x40;
+
+	malloc_fail:; //we have to allocate quite a bit of heap so this goto is useful here for oneline if statements
+		DBG(printf("malloc failed\n"););
+		// free_elements(elemlistp);
+		return (0);
+}
+
+static size_t process_post_frame_update(uint8_t *p, post_frame_update_t *postframep)
+{
+	postfifullblock_t *ibp = (postfifullblock_t *)p;
+	if (ibp->event_type != EVENT_POST_FRAME_UPDATE) return (0);
+
+	if (postframep == NULL) goto malloc_fail;
+
+	postframep->frame_number = (int32_t)ntohl(ibp->frame_number);
+	postframep->player_index = ibp->player_index;
+	postframep->is_follower = (bool_t)ibp->is_follower;
+	postframep->intern_char_id = ibp->intern_char_id;
+	postframep->action_state_id = (uint16_t)ntohs(ibp->action_state_id);
+	postframep->x_position = ntohf(ibp->x_position);
+	postframep->y_position = ntohf(ibp->y_position);
+	postframep->facing_direction = ntohf(ibp->facing_direction);
+	postframep->damage_percent = ntohf(ibp->damage_percent);
+	postframep->shield_size = ntohf(ibp->shield_size);
+	postframep->last_hit_id = ibp->last_hit_id;
+	postframep->current_combo_count = ibp->current_combo_count;
+	postframep->last_hit_by = ibp->last_hit_by;
+	postframep->stocks_remaining = ibp->stocks_remaining;
+	postframep->action_state_frames = ntohf(ibp->action_state_frames);
+	postframep->state_bitflags_1 = ibp->state_bitflags_1;
+	postframep->state_bitflags_2 = ibp->state_bitflags_2;
+	postframep->state_bitflags_3 = ibp->state_bitflags_3;
+	postframep->state_bitflags_4 = ibp->state_bitflags_4;
+	postframep->state_bitflags_5 = ibp->state_bitflags_5;
+	postframep->misc_as = ntohf(ibp->misc_as);
+	postframep->ground_state = (bool_t)ibp->ground_state;
+	postframep->last_ground_id = (uint16_t)ntohs(ibp->last_ground_id);
+	postframep->jumps_remaining = ibp->jumps_remaining;
+	postframep->lcancel_status = ibp->lcancel_status;
+	postframep->hurtbox_collision_state = ibp->hurtbox_collision_state;
+	postframep->si_air_x_speed = ntohf(ibp->si_air_x_speed);
+	postframep->si_air_y_speed = ntohf(ibp->si_air_y_speed);
+	postframep->attack_air_x_speed = ntohf(ibp->attack_air_x_speed);
+	postframep->attack_air_y_speed = ntohf(ibp->attack_air_y_speed);
+	postframep->si_ground_x_speed = ntohf(ibp->si_ground_x_speed);
+
+	return 0x49;
 
 	malloc_fail:; //we have to allocate quite a bit of heap so this goto is useful here for oneline if statements
 		DBG(printf("malloc failed\n"););
